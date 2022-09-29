a846afa025fbc1588906402896e81b9058200621
thandh <do.hoang.than@sun-asterisk.com>
2022-09-29 16:19:33 +0700

    import template

---
+++ Assets/Lobby and Worlds/Scripts/Clients/PlayerSettings.cs
@@ -0,0 +1,41 @@
+﻿
+
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using System;
+using UnityEngine;
+
+namespace FirstGearGames.LobbyAndWorld.Clients
+{
+
+    public class PlayerSettings : NetworkBehaviour
+    {
+
+        #region Private.
+        /// <summary>
+        /// Username for this client.
+        /// </summary>
+        [SyncVar]
+        private string _username;
+        #endregion
+
+        /// <summary>
+        /// Sets Username.
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetUsername(string value)
+        {
+            _username = value;
+        }
+        /// <summary>
+        /// Returns Username.
+        /// </summary>
+        /// <returns></returns>
+        public string GetUsername() 
+        {
+            return _username; 
+        }
+
+    }
+
+}
